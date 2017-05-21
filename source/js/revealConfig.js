6d2b0c21c9f798122f66fd9b087dca56412f5ddc
Tamara Temple <tamouse@gmail.com>
2017-05-21 18:12:48 -0500

    updates

--- source/js/revealConfig.js
+++ source/js/revealConfig.js
@@ -3,10 +3,11 @@
 Reveal.initialize({
   slideNumber: true,
   history: true,
-  progress: false,
+  progress: true,
   width: "100%",
   height: "100%",
   margin: "0.1",
   minScale: "1",
-  maxScale: "1"
+  maxScale: "1",
+  transition: 'none'
 });
